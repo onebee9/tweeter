447945c487985692497648fb3171b7fe5522ab96
ibifuro odu <oduibifuro@gmail.com>
2022-02-02 18:35:01 -0500

    Cleaned up files and added screenshots

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,19 +13,19 @@ $(document).ready(function () {
     const formData = $(this).serialize();
     let textAreaStatus = $("#tweet-text").val().trim();
 
-    if(textAreaStatus.length < 1){
+    if (textAreaStatus.length < 1) {
       $('.form-error')
         .html('<i class="fa fa-exclamation-triangle"></i>&nbsp;Please enter some text to tweet&nbsp;<i class="fa fa-exclamation-triangle"></i>')
         .slideDown(400);
     }
 
-    else if(textAreaStatus.length > 140){
+    else if (textAreaStatus.length > 140) {
       $('.form-error')
         .html('<i class="fa fa-exclamation-triangle"></i>&nbsp;Tweet is too long. Consider shortening your tweet.&nbsp;<i class="fa fa-exclamation-triangle"><i>')
         .slideDown(400);
     }
 
-    else{
+    else {
       //sends tweet to post route for processing + saving
       $.ajax({
         url: "/tweets",
@@ -39,34 +39,33 @@ $(document).ready(function () {
         error: function (response) {
         }
       });
-    }    
-});
-//end submit function
-
-// makes a request to the get route to retrieve all tweets, runs everytime the page loads
-const loadTweets = function () {
-  $.ajax({
-    url: "/tweets",
-    method: "GET",
-    success: function (data, textStatus, jqXHR) {
-
-      for (let i = data.length - 1; i >= 0; i--) {
-        // use function to create and append retrieved tweets
-        createTweetElement(data[i]);
-      }
-    },
-    error: function (data, textStatus, jqXHR) {
     }
   });
-}
+  //end submit function
 
-loadTweets();
+  // makes a request to the get route to retrieve all tweets, runs everytime the page loads
+  const loadTweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function (data, textStatus, jqXHR) {
+        for (let i = data.length - 1; i >= 0; i--) {
+          // creates and appends retrieved tweets
+          createTweetElement(data[i]);
+        }
+      },
+      error: function (data, textStatus, jqXHR) {
+      }
+    });
+  }
 
-const createTweetElement = function (tweetData) {
-  const imagedisplay = (tweetData.user.avatars && tweetData.user.avatars !== "") ? `<img alt="tweet-avatar" src="${tweetData.user.avatars}">` : `<span class ="fa-solid fa-user-astronaut"> </span>`;
-  const timeDisplay = timeago.format(tweetData.created_at);
+  loadTweets();
 
-  const tweet = `<article>
+  const createTweetElement = function (tweetData) {
+    const imagedisplay = (tweetData.user.avatars && tweetData.user.avatars !== "") ? `<img alt="tweet-avatar" src="${tweetData.user.avatars}">` : `<span class ="fa-solid fa-user-astronaut"> </span>`;
+    const timeDisplay = timeago.format(tweetData.created_at);
+
+    const tweet = `<article>
       <header class="articles-header">
         <div class = "profile-details">
           ${imagedisplay}
@@ -92,23 +91,6 @@ const createTweetElement = function (tweetData) {
       </footer>
     </article>`;
 
-  $('.tweet-listing').append(tweet);
-}
-
-  /*
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
+    $('.tweet-listing').append(tweet);
   }
-
-  createTweetElement(tweetData);
-  */
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $(".new-tweet form textarea").on("input", function() {
+$(document).ready(function () {
+  $(".new-tweet form textarea").on("input", function () {
     var totalLength = $(this).data("maxlength");
     var currentLength = $(this).val().length;
-  
-    if (currentLength > totalLength){
+
+    if (currentLength > totalLength) {
       $(".counter").css('cssText', 'color:red');
       $('.form-error')
         .html('<i class="fa fa-exclamation-triangle"></i>&nbsp;Tweet is too long. Consider shortening your tweet.</i>&nbsp;<i class="fa fa-exclamation-triangle"></i>')
         .slideDown(400);
     }
-    else{
+    else {
       $('.form-error').slideUp(400);
       $(".counter").css('cssText', 'color:#545149');
     }
